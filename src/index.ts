697b5e59800bf5ce6f337adc0ab265fe24c6b117
druid-sh <dev@druid.sh>
2025-09-27 13:54:09 +0200

    Remove loaders

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { DruidClient } from "./client";
 export { BlogList } from "./components/BlogList";
-export { BlogListLoading } from "./components/BlogListLoading";
 export { BlogPost } from "./components/BlogPost";
-export { BlogPostLoading } from "./components/BlogPostLoading";
 export {
   generateBlogPostMetadata,
   generateBlogListMetadata,
